1b509cb946573e23fcbc2561fac3600dc86f65f3
Heri Setiawan <skyhack86@gmail.com>
2019-12-07 06:43:16 +0700

    Initial commit

---
+++ app/src/main/java/com/alhanifdev/modulprj01/MainActivity.java
@@ -0,0 +1,20 @@
+package com.alhanifdev.modulprj01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //TO DO (5) inisialisasi button dan url dr gambar yang akan diakses
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TO DO (6) beri event pada button
+        // untuk panggil downloadasynctask
+    }
+
+    //TO DO (7) buat kelas downloadasynctask untuk download data
+
+    //TO DO (8) buat method untuk buka koneksi dg web server
+}
